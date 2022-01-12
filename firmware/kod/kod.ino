c83b9644163ce9ae79d3d4ce3653fb5c732c5413
BeNNeTTcik <mateuszdampc99@gmail.com>
2022-01-12 19:11:36 +0100

    BeNNeTT

---
+++ firmware/kod/kod.ino
@@ -0,0 +1,45 @@
+#define Rpin 11
+#define Gpin 10
+#define Bpin 9
+#define czujnik_sygnal A0
+
+
+float wartosc=0, sygnal_zfiltrowany = 0, filtr[] = {21, 26};
+void setup () { 
+  Serial.begin (9600);
+  pinMode(czujnik_sygnal, INPUT);
+  pinMode(Gpin, OUTPUT);
+  pinMode(Rpin, OUTPUT);
+  pinMode(Bpin, OUTPUT);
+}
+
+void loop () {
+  wartosc=analogRead(czujnik_sygnal)*(5.0/128.0);
+  
+  Serial.print(wartosc);
+  Serial.print(" ");
+  FiltrLP(wartosc);
+  
+  Serial.println(sygnal_zfiltrowany);
+  
+  if(sygnal_zfiltrowany>filtr[1]){
+      digitalWrite(Rpin,HIGH);
+      digitalWrite(Bpin,LOW);
+      digitalWrite(Gpin,LOW);
+      delay(1);
+    } else if(sygnal_zfiltrowany>filtr[0] && sygnal_zfiltrowany<filtr[1]){
+      digitalWrite(Gpin,HIGH);
+      digitalWrite(Bpin,LOW);
+      digitalWrite(Rpin,LOW);
+      delay(1);
+    } else if(sygnal_zfiltrowany<filtr[0]){
+      digitalWrite(Bpin,HIGH);
+      digitalWrite(Rpin,LOW);
+      digitalWrite(Gpin,LOW);
+      delay(1);
+    }
+}
+
+void FiltrLP(float sygnal) {
+  sygnal_zfiltrowany = (0.945*sygnal_zfiltrowany) + (0.0549*sygnal);
+}
